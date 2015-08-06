ab707ff8e330197edc498633704749682171a3be
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2015-08-06 18:58:15 +0800

    Troubleshooting the resizing & projection

--- viewerwidget.cpp
+++ viewerwidget.cpp
@@ -8,18 +8,19 @@
 ViewerWidget::ViewerWidget(QWidget *parent) :
     QOpenGLWidget(parent),
     ui(new Ui::ViewerWidget),
-    fileGetter(parent)
+    fileGetter(parent),
+    alpha_step(0.001f)
 {
     ui->setupUi(this);
+
+    alpha = 0.0f;
 
     fileGetter.setDirectory(QDir::homePath());
     //fileGetter.setDirectory(QDir::currentPath());
     fileGetter.setFileMode(QFileDialog::ExistingFiles); // to select all the input files at one go
     fileGetter.setNameFilter("*.vtk");
     if (fileGetter.exec())
-    {
         meshProc.loadFilesToMeshes(fileGetter.selectedFiles());
-    }
 
 }
 
@@ -38,13 +39,40 @@ void ViewerWidget::initializeGL()
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    float scalar = 1.2f * 0.5f;
-    left = scalar*(meshProc.cornerMin.x-meshProc.cornerMax.x);
-    right = scalar*(meshProc.cornerMax.x-meshProc.cornerMin.x);
-    bottom = scalar*(meshProc.cornerMin.y-meshProc.cornerMax.y);
-    top = scalar*(meshProc.cornerMax.y-meshProc.cornerMin.y);
-    nearVal = meshProc.cornerMax.z-meshProc.cornerMin.z;
-    farVal = 3.0f*(meshProc.cornerMax.z-meshProc.cornerMin.z;
+
+    if (meshProc.meshList.size()>0)
+    {
+        float scalar = 1.0f * 0.5f;
+        right = scalar*(meshProc.cornerMax.x-meshProc.cornerMin.x);
+        top = scalar*(meshProc.cornerMax.y-meshProc.cornerMin.y);
+        float depth = scalar*(meshProc.cornerMax.z-meshProc.cornerMin.z);
+        //set the frustum size to the largest dimension
+        if (right>top)
+        {
+            if (right>depth)
+                top = depth = right;
+            else
+                top = right = depth;
+        }
+        else
+        {
+            if (top>depth)
+                right = depth = top;
+            else
+                top = right = depth;
+        }
+        left = bottom = -right;
+        nearVal = depth/scalar; // field of view = 90 degrees
+        farVal = 2.0f*nearVal;
+    }
+    else
+    {
+        right = top = 1.0f;
+        left = bottom = -1.0f;
+        nearVal = 0.0f;
+        farVal = 1.0f;
+    }
+
     glFrustum(left, right, bottom, top, nearVal, farVal);
 
     glMatrixMode(GL_MODELVIEW);
@@ -54,20 +82,36 @@ void ViewerWidget::initializeGL()
 
 void ViewerWidget::resizeGL(int w, int h)
 {
-    float   origAspectRatio = (meshProc.cornerMax.x-meshProc.cornerMin.x)/(meshProc.cornerMax.y-meshProc.cornerMin.y),
+    float   origAspectRatio = (right-left)/(top-bottom),
             newAspectRatio = (float)w/h;
 
     if (newAspectRatio > origAspectRatio)
     {
-        // expand left&right, retain height
-        left *= newAspectRatio;
-        right *= newAspectRatio;
+        if (newAspectRatio < 1)
+        {
+            // shrink
+        }
+        else
+        {
+            // expand left&right, retain height & depth
+            right = 0.5f*newAspectRatio*(top-bottom);
+            left = -right;
+        }
+
     }
     else
     {
-        // expand bottom&top, retain width
-        top /= newAspectRatio;
-        bottom /= newAspectRatio;
+        if (newAspectRatio < 1)
+        {
+
+        }
+        else
+        {
+            // expand top&bottom, retain width & depth
+            top = 0.5f*(right-left)/newAspectRatio;
+            bottom = -top;
+        }
+
     }
 
     glMatrixMode(GL_PROJECTION);
@@ -75,34 +119,48 @@ void ViewerWidget::resizeGL(int w, int h)
     glFrustum(left, right, bottom, top, nearVal, farVal);
 
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 
 }
 
 void ViewerWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 1.0, 1.0);       //blue
+    glClearColor(0.0, 0.0, 0.0, 1.0);       //black
 
     //test
-    glBegin(GL_TRIANGLES);
-        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);  //red
-        glVertex3f(-1.0f, -1.0f, 0.2f);
-        glVertex3f(1.0f, -1.0f, 0.2f);
-        glVertex3f(0.0f, 1.0f, 0.2f);
+    if (meshProc.meshList.size()<1)
+    {
+        glBegin(GL_TRIANGLES);
+            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);  //red
+            glVertex3f(-1.0f, -1.0f, 0.2f);
+            glVertex3f(1.0f, -1.0f, 0.2f);
+            glVertex3f(0.0f, 1.0f, 0.2f);
 
-        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);  //green
-        glVertex3f(-1.0f, -1.0f, 0.5f);
-        glVertex3f(1.0f, -1.0f, 0.5f);
-        glVertex3f(0.0f, 1.0f, 0.5f);
+            glColor4f(0.0f, 1.0f, 0.0f, 1.0f);  //green
+            glVertex3f(-1.0f, -1.0f, 0.5f);
+            glVertex3f(1.0f, -1.0f, 0.5f);
+            glVertex3f(0.0f, 1.0f, 0.5f);
 
-        glColor4f(1.0f, 1.0f, 0.0f, 1.0f);  //yellow
-        glVertex3f(-1.0f, -1.0f, 0.1f);
-        glVertex3f(1.0f, -1.0f, 0.1f);
-        glVertex3f(0.0f, 1.0f, 0.3f);
-    glEnd();
+            glColor4f(1.0f, 1.0f, 0.0f, 1.0f);  //yellow
+            glVertex3f(-1.0f, -1.0f, 0.1f);
+            glVertex3f(1.0f, -1.0f, 0.1f);
+            glVertex3f(0.0f, 1.0f, 0.3f);
+        glEnd();
+    }
 
-/*    GLfloat alpha = 0.5f;   // 1 = opaque, 0 = transparent
+    glPushMatrix();
+
+    // move the object to a distance from the camera dynamically to put the object in the centre of the current frustum
+    glTranslatef(0.0f, 0.0f, -0.5f*(farVal+nearVal));
+
+    // reorient the object to see from its side assuming the default view is from the top
+    glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+
+    // move the object to position its center to the origin
+    glTranslatef(-0.5f*(meshProc.cornerMin.x+meshProc.cornerMax.x),
+                 -0.5f*(meshProc.cornerMin.y+meshProc.cornerMax.y),
+                 -0.5f*(meshProc.cornerMin.z+meshProc.cornerMax.z));
+
     for (int i=0; i<meshProc.meshList.size(); i++)
     {
         feMesh* curMesh = meshProc.meshList[i];
@@ -117,7 +175,9 @@ void ViewerWidget::paintGL()
             glEnd();
         }
     }
-*/
+    alpha = (alpha==1.0f?0.0f:alpha+alpha_step);
+
+    glPopMatrix();
 }
 
 
--- viewerwidget.h
+++ viewerwidget.h
@@ -27,6 +27,8 @@ private:
     QFileDialog fileGetter;
     MeshProcessor meshProc;
     double left, right, top, bottom, nearVal, farVal;
+    GLfloat alpha;   // 1 = opaque, 0 = transparent
+    const GLfloat alpha_step;
 };
 
 #endif // VIEWERWIDGET_H
